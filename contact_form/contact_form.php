8f3ff4dd2b1833edfca2af50077bae3831a0b213
alaminjsr <45663445+alaminjsr@users.noreply.github.com>
2018-12-09 10:46:40 +0600

    Create contact_form.php

---
+++ contact_form/contact_form.php
@@ -0,0 +1,23 @@
+﻿[php]
+
+<?php
+// Naming Constant
+define( "RECIPIENT_NAME", "Alamin" );
+define( "RECIPIENT_EMAIL", "alaminhowladerofficial@gmail.com" );
+// Values
+$success = false;
+$senderName = isset( $_POST[‘username’] ) ? preg_replace( "/[^\.\-\’ a-zA-Z0-9]/", "", $_POST[‘username’] ) : "";
+$senderEmail = isset( $_POST[’email’] ) ? preg_replace( "/[^\.\-\_\@a-zA-Z0-9]/", "", $_POST[’email’] ) : "";
+$subject = isset( $_POST[‘subject’] ) ? preg_replace( "/[^\.\-\’ a-zA-Z0-9]/", "", $_POST[‘subject’] ) : "";
+$message = isset( $_POST[‘message’] ) ? preg_replace( "/(From:|To:|BCC:|CC:|Subject:|Content-Type:)/", "", $_POST[‘message’] ) : "";
+// Send the email
+if ( $senderName &amp;amp;&amp;amp; $senderEmail &amp;amp;&amp;amp; $message ) {
+$recipient = RECIPIENT_NAME . " &amp;lt;" . RECIPIENT_EMAIL . "&amp;gt;";
+$headers = "From: " . $senderName . " &amp;lt;" . $senderEmail . "&amp;gt;";
+$success = mail( $recipient, $subject, $message, $headers );
+//Set Location
+header(‘Location: index.html’);
+}
+?>
+
+[/php]
